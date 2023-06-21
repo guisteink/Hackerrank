e6adf4cef7847464a555c79e3839203fb23121b4
Guilherme <guilherme.stein@zenvia.com>
2023-06-20 23:44:36 -0300

    remove

--- Testes técnicos/neoway-validation/src/theme/theme.js
+++
@@ -1,6 +0,0 @@
-import { extendTheme } from "@chakra-ui/react";
-import { CardComponent } from "./additions/card/Card";
-
-export default extendTheme(
-    CardComponent, // Card componPanel Container component
-);
